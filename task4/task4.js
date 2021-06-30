a2717993eab143958c7d9a4136fa7d38b944d6c8
yulchik-sh <julasha_sh@mail.ru>
2021-06-30 22:51:27 +0300

    выполнены 4 задания из 6

---
+++ task4/task4.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/**
+ * Функция складывает 2 переданных параметра
+ * @param {number} a - первая переменная
+ * @param {number} b - первая переменная
+ * @returns {number} сумма переданных параметров
+ */
+function addition(a, b) {
+    return a + b;
+}
+
+/**
+ * Функция вычитает 2 переданных параметра
+ * @param {number} a - первая переменная
+ * @param {number} b - первая переменная
+ * @returns {number} разность переданных параметров
+ */
+function subtraction(a, b) {
+    return a - b;
+}
+
+/**
+ * Функция умножает 2 переданных параметра
+ * @param {number} a - первая переменная
+ * @param {number} b - первая переменная
+ * @returns {number} произведение переданных параметров
+ */
+function multiplication(a, b) {
+    return a * b;
+}
+
+/**
+ * Функция делит 2 переданных параметра
+ * @param {number} a - первая переменная
+ * @param {number} b - первая переменная
+ * @returns {number} частное переданных параметров
+ */
+function division(a, b) {
+    return a / b;
+}
